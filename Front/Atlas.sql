86c367e0551f99ddff5aa7be798912f0e7358dcb
AlbertoItuarte <158543574+AlbertoItuarte@users.noreply.github.com>
2024-12-16 18:22:46 -0600

    Update Atlas.sql

--- Front/Atlas.sql
+++ Front/Atlas.sql
@@ -28,11 +28,13 @@ CREATE TABLE IF NOT EXISTS Clientes (
 CREATE TABLE IF NOT EXISTS EvaluacionesFisicas (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     IdCliente INT NOT NULL,
+    IdCoach INT NOT NULL,
     FechaEval DATE NOT NULL,
     Peso DECIMAL(5,2),
     PorcentGrasa DECIMAL(5,2),
     Notas TEXT,
-    FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE
+    FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE,
+    FOREIGN KEY (IdCoach) REFERENCES Coaches(id)
 );
 
 -- Crear la tabla CategoriasEjercicios
@@ -55,9 +57,12 @@ CREATE TABLE IF NOT EXISTS Ejercicios (
     Ejercicio VARCHAR(255) NOT NULL,
     IdMusculoObjetivo INT,
     IdCategoriaEjercicio INT,
+    IdCoach INT NOT NULL,
     FOREIGN KEY (IdMusculoObjetivo) REFERENCES MusculosObjetivos(Id),
-    FOREIGN KEY (IdCategoriaEjercicio) REFERENCES CategoriasEjercicios(Id)
+    FOREIGN KEY (IdCategoriaEjercicio) REFERENCES CategoriasEjercicios(Id),
+    FOREIGN KEY (IdCoach) REFERENCES Coaches(id)
 );
+
 
 -- Crear la tabla Horarios
 CREATE TABLE IF NOT EXISTS Horarios (
@@ -86,22 +91,25 @@ CREATE TABLE IF NOT EXISTS Contratos (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     IdCliente INT NOT NULL,
     IdPlan INT NOT NULL,
+    IdCoach INT NOT NULL,
     FechaInicio DATE NOT NULL,
     FechaFin DATE NOT NULL,
     Monto DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE,
-    FOREIGN KEY (IdPlan) REFERENCES Planes(Id)
+    FOREIGN KEY (IdPlan) REFERENCES Planes(Id),
+    FOREIGN KEY (IdCoach) REFERENCES Coaches(id)
 );
 
 -- Crear la tabla Pagos
 CREATE TABLE IF NOT EXISTS Pagos (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     IdContrato INT NOT NULL,
+    IdCoach INT NOT NULL,
     Monto DECIMAL(10, 2) NOT NULL,
     FechaPago DATE NOT NULL,
-    FOREIGN KEY (IdContrato) REFERENCES Contratos(Id) ON DELETE CASCADE
+    FOREIGN KEY (IdContrato) REFERENCES Contratos(Id) ON DELETE CASCADE,
+    FOREIGN KEY (IdCoach) REFERENCES Coaches(id)
 );
-
 -- Crear la tabla RutinasClientes
 CREATE TABLE IF NOT EXISTS RutinasClientes (
     Id INT AUTO_INCREMENT PRIMARY KEY,
@@ -137,12 +145,12 @@ VALUES
 ('Sofía', 'López', 'Ramírez', 1, 'sofia.lopez@yahoo.com', '5555678910', '2001-07-15'),
 ('Carlos', 'González', 'Torres', 1, 'carlos.gonzalez@outlook.com', '5552345678', '1993-02-28');
 
-INSERT INTO Horarios (IdCliente, DiaSemana, HoraInicio, HoraFin)
+INSERT INTO Horarios (IdCliente, DiaSemana, HoraInicio, HoraFin, IdCoach)
 VALUES
-(1, 'Lunes', '07:00', '09:00'),
-(1, 'Martes', '08:00', '10:00'),
-(1, 'Miércoles', '09:00', '11:00'),
-(1, 'Jueves', '10:00', '12:00');
+(1, 'Lunes', '07:00', '09:00', 1),
+(1, 'Martes', '08:00', '10:00', 1),
+(1, 'Miércoles', '09:00', '11:00', 1),
+(1, 'Jueves', '10:00', '12:00', 1);
 
 INSERT INTO CategoriasEjercicios (Categoria)
 VALUES
@@ -150,12 +158,12 @@ VALUES
 ('Jalon'),
 ('Pierna');
 
-INSERT INTO EvaluacionesFisicas (Id, IdCliente, FechaEval, Peso, PorcentGrasa, Notas)
+INSERT INTO EvaluacionesFisicas (Id, IdCliente, IdCoach, FechaEval, Peso, PorcentGrasa, Notas)
 VALUES
-(1, 1, '2024-12-05', 56, 15, 'Aumento de masa muscular'),
-(2, 2, '2024-12-05', 64, 18, 'Mantenimiento'),
-(3, 3, '2024-12-05', 80, 27, 'Disminuir grasa corporal'),
-(4, 4, '2024-12-05', 75, 22, 'Competencia');
+(1, 1, 1, '2024-12-05', 56, 15, 'Aumento de masa muscular'),
+(2, 2, 1, '2024-12-05', 64, 18, 'Mantenimiento'),
+(3, 3, 1, '2024-12-05', 80, 27, 'Disminuir grasa corporal'),
+(4, 4, 1, '2024-12-05', 75, 22, 'Competencia');
 
 INSERT INTO MusculosObjetivos (MusculoObjetivo, idCategoria)
 VALUES
@@ -170,55 +178,55 @@ VALUES
 ('Chamorro', 3),
 ('Glúteo', 3);
 
-INSERT INTO Ejercicios (Ejercicio, IdMusculoObjetivo, IdCategoriaEjercicio)
+INSERT INTO Ejercicios (Ejercicio, IdMusculoObjetivo, IdCategoriaEjercicio, IdCoach)
 VALUES
-('Press banca', 1, 1),
-('Peck Deck', 1, 1),
-('Press inclinado', 1, 1),
-('Press declinado', 1, 1),
-('Press con mancuernas', 1, 1),
-('Press francés', 2, 1),
-('Extensión trasnuca polea', 2, 1),
-('Extensión con soga', 2, 1),
-('Extensión con mancuernas', 2, 1),
-('Elevaciones laterales polea/máquina', 3, 1),
-('Elevaciones laterales mancuernas', 3, 1),
-('Press militar mancuernas/barra/máquina', 3, 1),
-('Remo con agarre amplio', 4, 2),
-('Peck Deck', 4, 2),
-('Jalon vertical', 4, 2),
-('Remo gironda', 4, 2),
-('Pull over', 4, 2),
-('Dominadas', 4, 2),
-('Dominadas asistidas', 4, 2),
-('Remo en máquina', 4, 2),
-('Curl de muñeca', 5, 2),
-('Curl de muñeca inverso', 5, 2),
-('Front lever', 5, 2),
-('Curl invertido', 5, 2),
-('Curl martillo', 5, 2),
-('Curl de Bicep', 6, 2),
-('Curl mesero', 6, 2),
-('Curl en máquina/polea', 6, 2),
-('Curl inclinado', 6, 2),
-('Curl araña', 6, 2),
-('Sentadilla Hack', 7, 3),
-('Sentadilla con barra', 7, 3),
-('Sentadilla en smith', 7, 3),
-('Búlgaras', 7, 3),
-('Extensiones de Cuadricep', 7, 3),
-('Prensa', 7, 3),
-('Curl acostado', 8, 3),
-('Curl sentado', 8, 3),
-('Curl de pie', 8, 3),
-('Peso muerto', 8, 3),
-('Elevaciones de pie', 9, 3),
-('Elevaciones sentado', 9, 3),
-('Hip thrust', 10, 3),
-('Patada de glúteo', 10, 3),
-('Búlgaras', 10, 3);
+('Press banca', 1, 1, 1),
+('Peck Deck', 1, 1, 1),
+('Press inclinado', 1, 1, 1),
+('Press declinado', 1, 1, 1),
+('Press con mancuernas', 1, 1, 1),
+('Press francés', 2, 1, 1),
+('Extensión trasnuca polea', 2, 1, 1),
+('Extensión con soga', 2, 1, 1),
+('Extensión con mancuernas', 2, 1, 1),
+('Elevaciones laterales polea/máquina', 3, 1, 1),
+('Elevaciones laterales mancuernas', 3, 1, 1),
+('Press militar mancuernas/barra/máquina', 3, 1, 1),
+('Remo con agarre amplio', 4, 2, 1),
+('Peck Deck', 4, 2, 1),
+('Jalon vertical', 4, 2, 1),
+('Remo gironda', 4, 2, 1),
+('Pull over', 4, 2, 1),
+('Dominadas', 4, 2, 1),
+('Dominadas asistidas', 4, 2, 1),
+('Remo en máquina', 4, 2, 1),
+('Curl de muñeca', 5, 2, 1),
+('Curl de muñeca inverso', 5, 2, 1),
+('Front lever', 5, 2, 1),
+('Curl invertido', 5, 2, 1),
+('Curl martillo', 5, 2, 1),
+('Curl de Bicep', 6, 2, 1),
+('Curl mesero', 6, 2, 1),
+('Curl en máquina/polea', 6, 2, 1),
+('Curl inclinado', 6, 2, 1),
+('Curl araña', 6, 2, 1),
+('Sentadilla Hack', 7, 3, 1),
+('Sentadilla con barra', 7, 3, 1),
+('Sentadilla en smith', 7, 3, 1),
+('Búlgaras', 7, 3, 1),
+('Extensiones de Cuadricep', 7, 3, 1),
+('Prensa', 7, 3, 1),
+('Curl acostado', 8, 3, 1),
+('Curl sentado', 8, 3, 1),
+('Curl de pie', 8, 3, 1),
+('Peso muerto', 8, 3, 1),
+('Elevaciones de pie', 9, 3, 1),
+('Elevaciones sentado', 9, 3, 1),
+('Hip thrust', 10, 3, 1),
+('Patada de glúteo', 10, 3, 1),
+('Búlgaras', 10, 3, 1);
 
-INSERT INTO RutinasClientes (IdCliente, IdEjercicio, Series, Repeticiones)
+INSERT INTO RutinasClientes (IdCliente, IdEjercicio, Series, Repeticiones, Dia)
 VALUES
 (1, 3, 3, 12, 'Lunes'),
 (1, 13, 4, 8, 'Lunes'),
@@ -231,58 +239,57 @@ VALUES
 (1, 13, 3, 10),
 (1, 36, 3, 12);
 
-INSERT INTO Planes (IdCoach, Plan, Duración, Costo)
+INSERT INTO Planes (IdCoach, Plan, Duracion, Costo)
 VALUES
 (1, 'Básico', 1, 500),
 (1, 'Intermedio', 3, 1400),
 (1, 'Avanzado', 6, 2500),
 (1, 'Premium', 12, 4000);
 
-INSERT INTO Contratos (IdCliente, IdPlan, FechaInicio, FechaFin)
+INSERT INTO Contratos (IdCliente, IdPlan, IdCoach, FechaInicio, FechaFin, Monto)
 VALUES
-(1, 1, '2024-01-01', '2024-03-31'),
-(2, 2, '2024-02-01', '2024-04-30'),
-(3, 1, '2024-03-01', '2024-05-31'),
-(4, 3, '2024-01-15', '2024-06-14');
+(1, 1, 1, '2024-01-01', '2024-03-31', 500),
+(2, 2, 1, '2024-02-01', '2024-04-30', 1400),
+(3, 1, 1, '2024-03-01', '2024-05-31', 500),
+(4, 3, 1, '2024-01-15', '2024-06-14', 2500);
 
-INSERT INTO Pagos (IdContrato, Monto, FechaPago)
+INSERT INTO Pagos (IdContrato, IdCoach, Monto, FechaPago)
 VALUES
-(1, 500, '2024-01-15'),
-(2, 500, '2024-02-15'),
-(3, 500, '2024-03-15'),
-(4, 1400, '2024-01-15');
+(1, 1, 500, '2024-01-15'),
+(2, 1, 1400, '2024-02-15'),
+(3, 1, 500, '2024-03-15'),
+(4, 1, 2500, '2024-01-15');
+-- ALTER TABLE EvaluacionesFisicas
+-- DROP FOREIGN KEY EvaluacionesFisicas_ibfk_1;
 
-ALTER TABLE EvaluacionesFisicas
-DROP FOREIGN KEY EvaluacionesFisicas_ibfk_1;
+-- ALTER TABLE EvaluacionesFisicas
+-- ADD CONSTRAINT EvaluacionesFisicas_ibfk_1
+-- FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE;
 
-ALTER TABLE EvaluacionesFisicas
-ADD CONSTRAINT EvaluacionesFisicas_ibfk_1
-FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE;
+-- ALTER TABLE Horarios
+-- DROP FOREIGN KEY Horarios_ibfk_1;
 
-ALTER TABLE Horarios
-DROP FOREIGN KEY Horarios_ibfk_1;
+-- ALTER TABLE Horarios
+-- ADD CONSTRAINT Horarios_ibfk_1
+-- FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE;
 
-ALTER TABLE Horarios
-ADD CONSTRAINT Horarios_ibfk_1
-FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE;
+-- ALTER TABLE Contratos
+-- DROP FOREIGN KEY Contratos_ibfk_1;
 
-ALTER TABLE Contratos
-DROP FOREIGN KEY Contratos_ibfk_1;
+-- ALTER TABLE Contratos
+-- ADD CONSTRAINT Contratos_ibfk_1
+-- FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE;
 
-ALTER TABLE Contratos
-ADD CONSTRAINT Contratos_ibfk_1
-FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE;
+-- ALTER TABLE Pagos
+-- DROP FOREIGN KEY Pagos_ibfk_1;
 
-ALTER TABLE Pagos
-DROP FOREIGN KEY Pagos_ibfk_1;
+-- ALTER TABLE Pagos
+-- ADD CONSTRAINT Pagos_ibfk_1
+-- FOREIGN KEY (IdContrato) REFERENCES Contratos(Id) ON DELETE CASCADE;
 
-ALTER TABLE Pagos
-ADD CONSTRAINT Pagos_ibfk_1
-FOREIGN KEY (IdContrato) REFERENCES Contratos(Id) ON DELETE CASCADE;
+-- ALTER TABLE RutinasClientes
+-- DROP FOREIGN KEY RutinasClientes_ibfk_1;
 
-ALTER TABLE RutinasClientes
-DROP FOREIGN KEY RutinasClientes_ibfk_1;
-
-ALTER TABLE RutinasClientes
-ADD CONSTRAINT RutinasClientes_ibfk_1
-FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE;
+-- ALTER TABLE RutinasClientes
+-- ADD CONSTRAINT RutinasClientes_ibfk_1
+-- FOREIGN KEY (IdCliente) REFERENCES Clientes(Id) ON DELETE CASCADE;
