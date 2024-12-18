e7f3a48c4db3a5affd59a9e7fe5823bee39ef83b
AlbertoItuarte <158543574+AlbertoItuarte@users.noreply.github.com>
2024-12-17 20:41:49 -0600

    fin

--- Front/app/Contratos.js
+++ Front/app/Contratos.js
@@ -11,6 +11,16 @@ import Screen from "../components/Screen";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Print from "expo-print";
+import * as Sharing from "expo-sharing";
+import * as FileSystem from "expo-file-system";
+
+const ensureDirExists = async (dir) => {
+  const dirInfo = await FileSystem.getInfoAsync(dir);
+  if (!dirInfo.exists) {
+    await FileSystem.makeDirectoryAsync(dir, { intermediates: true });
+  }
+};
 
 export default function Contratos() {
   const [contratos, setContratos] = useState([]);
@@ -50,6 +60,76 @@ export default function Contratos() {
     fetchData();
   }, []);
 
+  // Obtener la fecha actual
+  const fechaActual = new Date();
+
+  // Filtrar los contratos cuya fecha de finalización no ha pasado
+  const contratosVigentes = contratos.filter(
+    (contrato) => new Date(contrato.FechaFin) >= fechaActual
+  );
+
+  const generarPDF = async () => {
+    try {
+      console.log("Generando PDF...");
+      let htmlContent = `
+        <h1>Lista de Contratos</h1>
+        <table border="1" style="width: 100%; border-collapse: collapse;">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Inicio</th>
+              <th>Final</th>
+              <th>Plan</th>
+              <th>Costo</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${contratosVigentes
+              .map(
+                (contrato) => `
+              <tr>
+                <td>${contrato.NombreCliente} ${contrato.ApellidoPCliente}</td>
+                <td>${new Date(contrato.FechaInicio).toISOString().split("T")[0]}</td>
+                <td>${new Date(contrato.FechaFin).toISOString().split("T")[0]}</td>
+                <td>${contrato.NombrePlan}</td>
+                <td>${parseInt(contrato.Monto, 10)}</td>
+              </tr>
+            `
+              )
+              .join("")}
+          </tbody>
+        </table>
+      `;
+
+      const { uri } = await Print.printToFileAsync({ html: htmlContent });
+      console.log("PDF generado en:", uri);
+
+      // Define la ruta personalizada
+      const newUri = `${FileSystem.documentDirectory}contratos/lista_contratos.pdf`;
+
+      // Asegúrate de que el directorio exista
+      await ensureDirExists(`${FileSystem.documentDirectory}contratos`);
+
+      // Mueve el archivo a la nueva ubicación
+      await FileSystem.moveAsync({
+        from: uri,
+        to: newUri,
+      });
+
+      console.log("PDF movido a:", newUri);
+      // alert(`PDF generado en: ${newUri}`);
+
+      if (await Sharing.isAvailableAsync()) {
+        await Sharing.shareAsync(newUri);
+      } else {
+        alert("La función de compartir no está disponible en este dispositivo");
+      }
+    } catch (error) {
+      console.error("Error al generar el PDF:", error);
+      alert("Error al generar el PDF");
+    }
+  };
+
   return (
     <Screen>
       <View className="flex flex-col items-center">
@@ -64,8 +144,8 @@ export default function Contratos() {
               <Text className="text-white w-1/5 text-center">Costo</Text>
             </View>
             <View className="pb-40 bg-gray-800">
-              {Array.isArray(contratos) &&
-                contratos.map((contrato, index) => (
+              {Array.isArray(contratosVigentes) &&
+                contratosVigentes.map((contrato, index) => (
                   <TouchableHighlight
                     key={index.toString()}
                     onLongPress={() => console.log("Editar", contrato)}
@@ -102,10 +182,7 @@ export default function Contratos() {
                     </View>
                   </TouchableHighlight>
                 ))}
-              <Button
-                title="Generar PDF"
-                onPress={() => console.log("Generar PDF")}
-              />
+              <Button title="Generar PDF" onPress={generarPDF} />
             </View>
           </View>
         </ScrollView>
